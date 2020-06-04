5c20c90aa84a66c88a913bf96552f3eb3a128853
BlueGuy5 <55474089+BlueGuy5@users.noreply.github.com>
2020-06-04 09:41:09 -0700

    Initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Read_logs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_ReadLogs = new System.Windows.Forms.TextBox();
+            this.btn_reload = new System.Windows.Forms.Button();
+            this.txt_search = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_ReadLogs
+            // 
+            this.txt_ReadLogs.Location = new System.Drawing.Point(12, 49);
+            this.txt_ReadLogs.Multiline = true;
+            this.txt_ReadLogs.Name = "txt_ReadLogs";
+            this.txt_ReadLogs.ReadOnly = true;
+            this.txt_ReadLogs.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_ReadLogs.Size = new System.Drawing.Size(776, 389);
+            this.txt_ReadLogs.TabIndex = 0;
+            // 
+            // btn_reload
+            // 
+            this.btn_reload.Location = new System.Drawing.Point(579, 12);
+            this.btn_reload.Name = "btn_reload";
+            this.btn_reload.Size = new System.Drawing.Size(76, 31);
+            this.btn_reload.TabIndex = 1;
+            this.btn_reload.Text = "Reload";
+            this.btn_reload.UseVisualStyleBackColor = true;
+            this.btn_reload.Click += new System.EventHandler(this.btn_reload_ClickAsync);
+            // 
+            // txt_search
+            // 
+            this.txt_search.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_search.Location = new System.Drawing.Point(12, 17);
+            this.txt_search.Name = "txt_search";
+            this.txt_search.Size = new System.Drawing.Size(561, 26);
+            this.txt_search.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btn_reload;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_search);
+            this.Controls.Add(this.btn_reload);
+            this.Controls.Add(this.txt_ReadLogs);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_ReadLogs;
+        private System.Windows.Forms.Button btn_reload;
+        private System.Windows.Forms.TextBox txt_search;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Read_logs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+            //FileInfo Originalfile = new FileInfo(@"C:\Users\williamyu\Desktop\Alexa Logs\OOR_BT_Log_Test.txt");
+            //FileInfo CopyFile = new FileInfo(@"C:\Users\williamyu\Desktop\Alexa Logs\OOR_BT_Log_Test - Copy.txt");
+
+            //if (Originalfile.Length > CopyFile.Length)
+            //{
+                //txt_ReadLogs.Text = "";
+                //var readlog = System.IO.File.ReadAllLines(@"C:\Users\williamyu\Desktop\Alexa Logs\OOR_BT_Log_Test.txt");
+                if (System.IO.File.Exists(@"C:\Users\williamyu\Desktop\OTA - VS.txt"))
+                {
+                    System.IO.File.Delete(@"C:\Users\williamyu\Desktop\OTA - VS.txt");
+                    //System.IO.File.WriteAllLines(@"C:\Users\williamyu\Desktop\Alexa Logs\OOR_BT_Log_Test - Copy.txt", readlog);
+                }
+                //else
+                //{
+                System.IO.File.Copy(@"C:\Users\williamyu\Desktop\OTA.txt", @"C:\Users\williamyu\Desktop\OTA - VS.txt");
+                //}
+
+                var readcopylog = System.IO.File.ReadAllLines(@"C:\Users\williamyu\Desktop\OTA - VS.txt");
+                foreach (string lines in readcopylog)
+                {
+                    if (lines.Contains(txt_search.Text))
+                    {
+                        txt_ReadLogs.AppendText(lines + "\r\n");
+                    }
+                }
+            //}
+            */
+            txt_search.Text = "reconnect attempt";
+        }
+        private async Task test_log()
+        {
+            string line;
+            string file = @"C:\Users\williamyu\Desktop\BT.txt";
+            bool cont = true;
+
+            txt_ReadLogs.Text = "";
+            using (FileStream stream = File.Open(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (StreamReader reader = new StreamReader(stream))
+                while (cont == true)
+                {
+                    while ((line = await reader.ReadLineAsync()) != null)
+                    {
+                        if (line.Contains(txt_search.Text))
+                        {
+                            txt_ReadLogs.AppendText(line + "\r\n");
+                        }
+                    }
+                }
+        }
+        private async void btn_reload_ClickAsync(object sender, EventArgs e)
+        {
+
+            await test_log();
+            /*
+            int counter = 0;
+            string line;
+            while (counter == 0)
+            {
+                txt_ReadLogs.Text = "";
+                using (FileStream stream = File.Open(@"C:\Users\williamyu\Desktop\BT.txt", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using (StreamReader reader = new StreamReader(stream))
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line.Contains(txt_search.Text))
+                        {
+                            txt_ReadLogs.AppendText(line + "\r\n");
+                            counter++;
+                        }
+                        counter = 0;
+                        //counter++;
+                    }
+                counter = 0;
+            }
+            */
+
+            /*
+            using (FileStream stream = File.Open(@"C:\Users\williamyu\Desktop\BT.txt", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    txt_ReadLogs.Text = "";
+                    while (!reader.EndOfStream)
+                    {
+                        
+                        if (reader.ReadLine().Contains(txt_search.Text))
+                        {
+                            txt_ReadLogs.AppendText(reader.ReadLine() + "\r\n");
+                        }  
+                        
+                        var readlog = System.IO.File.ReadAllLines(@"C:\Users\williamyu\Desktop\BT.txt");
+                        foreach (string lines in readlog)
+                        {
+                            if (lines.Contains(txt_search.Text))
+                            {
+                                txt_ReadLogs.AppendText(lines + "\r\n");
+                            }
+                        }
+                    }
+                }
+            }
+
+            */
+
+            /*
+            var infile = @"C:\Users\williamyu\Desktop\BT.txt";
+            var outfile = @"C:\Users\williamyu\Desktop\BT - VS.txt";
+            //FileInfo Originalfile = new FileInfo(@"C:\Users\williamyu\Desktop\Alexa Logs\OOR_BT_Log_Test.txt");
+            //FileInfo CopyFile = new FileInfo(@"C:\Users\williamyu\Desktop\Alexa Logs\OOR_BT_Log_Test - Copy.txt");
+
+            //while (Originalfile.Length > CopyFile.Length)
+            //{
+                txt_ReadLogs.Text = "";
+                if (System.IO.File.Exists(outfile))
+                {
+                    System.IO.File.Delete(outfile);
+                    //System.IO.File.WriteAllLines(@"C:\Users\williamyu\Desktop\Alexa Logs\OOR_BT_Log_Test - Copy.txt", readlog);
+                }
+                //else
+                //{
+                System.IO.File.Copy(infile, outfile);
+                //}
+
+                var readcopylog = System.IO.File.ReadAllLines(outfile);
+                foreach (string lines in readcopylog)
+                {
+                    if (lines.Contains(txt_search.Text))
+                    {
+                        txt_ReadLogs.AppendText(lines + "\r\n");
+                    }
+                }  
+                */
+        }
+    }     
+}
